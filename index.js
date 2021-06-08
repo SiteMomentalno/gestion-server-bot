aac5ac8345a75633cb601493009ae98b92da60b8
SiteMomentalno <72298763+SiteMomentalno@users.noreply.github.com>
2021-06-08 10:02:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,66 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "!";
 
-client.once('ready', () => {
+client.once('ready', function() {
    console.log(`Félicitations, votre bot Discord a été correctement initialisé !`);
 });
 
-client.on(`message`, message => {
-    if (message.content === `ping`) {
-      message.channel.send(`Pong.`)
-    }
+client.on(`message`, function(message) {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === "ping") {
+      const timeTaken = Date.now() - message.createdTimestamp;
+      message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);                      
+    }  
 });
 
-client.on("guildMemberAdd", async member => {
-  const channel = member.guild.channels.cache.find(
-    ch => ch.name === "member-log"
-  )
-  if (!channel) return
-
-  const canvas = Canvas.createCanvas(700, 250)
-  const ctx = canvas.getContext("2d")
-
-  const background = await Canvas.loadImage("./wallpaper.jpg")
-  ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
-
-  ctx.strokeStyle = "#74037b"
-  ctx.strokeRect(0, 0, canvas.width, canvas.height)
-
-  // Texte générique d'introduction avant le nom d'utilisateur
-  ctx.font = "28px sans-serif"
-  ctx.fillStyle = "#ffffff"
-  ctx.fillText(
-    "Bienvenue sur le serveur,",
-    canvas.width / 2.5,
-    canvas.height / 3.5
-  )
-
-  // Ajouter le nom d'utilisateur
-  ctx.font = applyText(canvas, `${member.displayName}`)
-  ctx.fillStyle = "#ffffff"
-  ctx.fillText(
-    `${member.displayName}!`,
-    canvas.width / 2.5,
-    canvas.height / 1.8
-  )
-
-  ctx.beginPath()
-  ctx.arc(125, 125, 100, 0, Math.PI * 2, true)
-  ctx.closePath()
-  ctx.clip()
-
-  const avatar = await Canvas.loadImage(
-    member.user.displayAvatarURL({ format: "jpg" })
-  )
-  ctx.drawImage(avatar, 25, 25, 200, 200)
-
-  const attachment = new Discord.MessageAttachment(
-    canvas.toBuffer(),
-    "bienvenue-image.png"
-  )
-
-  channel.send(`Bienvenue sur le serveur, ${member}!`, attachment)
-});
 
 client.login(process.env.TOKEN);
 
